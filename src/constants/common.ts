69b4354acbc58f5f4d4ab16ca1c4cb011ed1da7f
sicnarf2 <francisreserva12@gmail.com>
2024-09-06 08:31:33 +0800

    aws profile resume

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Francis A. Reserva';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Skilled professional responsible for writing, testing, debugging, and maintaining software code to create functional applications and systems. Possess expertise in programming languages, algorithms, data structures, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Responsible for developing and maintaining web applications from the frontend to the backend, work closely with cross-functional teams to design, implement, and deploy scalable solutions that meet business objectives.';
+export const ABOUT_TEXT_3 =
+  'Versatile professional proficient in both frontend and backend development, capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Software Developer with 12+ years of experience building websites and web applications using modern JavaScript tools/frameworks. Depth knowledge of front-end and back-end technologies and database  management.';
+export const RESUME_TEXT_2 =
+  'Software Developer with 12+ years of experience building websites and web applications using modern JavaScript  tools/frameworks.';
